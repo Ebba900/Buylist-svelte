7ebcb16fdf72acb4495011ea88027fb881bb824d
Ebba900 <ebba900@gmail.com>
2021-05-26 16:53:01 +0200

    Buy list

---
+++ src/App.svelte
@@ -0,0 +1,45 @@
+<title> Inköpslista</title>
+<script>
+    let newItem = '';
+	
+    let thingsToBuy = [{text: '', status: true},
+                    {text: '', status: false},
+                    {text: '', status: false}];
+	
+	function addToList() {
+		thingsToBuy = [...thingsToBuy, {text: newItem, status: false}];
+		newItem = '';
+	}
+	
+	function removeFromList(index) {
+		thingsToBuy.splice(index, 1)
+		thingsToBuy = thingsToBuy;
+    }
+</script>
+<main>
+<input bind:value={newItem} type="text" placeholder="Nytt inköp..">
+<button on:click={addToList}>lägg till</button>
+</main>
+<br/>
+{#each thingsToBuy as item, index}
+	<input bind:checked={item.status} type="checkbox">
+	<span class:checked={item.status}>{item.text}</span>
+	<span on:click={() => removeFromList(index)}>Tabort</span>
+	<br/>
+{/each} 
+
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
